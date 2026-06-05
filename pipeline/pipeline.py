3d86de5d0327ff8aa132330a5040465dbef8b740
Furnx <89989017+Furnx@users.noreply.github.com>
2026-06-05 09:54:15 +0000

    venv and pandas added

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,3 +4,9 @@ print("arguments", sys.argv)
 day = int(sys.argv[1])
 
 print(f"Running pipeline for day {day}")
+
+import pandas as pd
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+
+print(df.head())
