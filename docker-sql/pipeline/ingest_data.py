9e3ed0ca58af3587c74699d6db20bcfd954bed84
Furnx <89989017+Furnx@users.noreply.github.com>
2026-06-09 20:44:00 +0000

    data ingestion script cleanup - checkpoint 1 (added click tool)

--- docker-sql/pipeline/ingest_data.py
+++ docker-sql/pipeline/ingest_data.py
@@ -32,16 +32,7 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-@click.command()
-@click.option('--pg-user', default='root', help='PostgreSQL user')
-@click.option('--pg-pass', default='root', help='PostgreSQL password')
-@click.option('--pg-host', default='localhost', help='PostgreSQL host')
-@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
-@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--year', default=2021, type=int, help='Year of the data')
-@click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
-@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+
 
 
 # Read a sample of the data
