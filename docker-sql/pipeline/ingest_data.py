5253ee6f00b880a1231ef1c96854bfa4e8264df2
Furnx <89989017+Furnx@users.noreply.github.com>
2026-06-09 20:37:37 +0000

    data ingestion script cleanup - checkpoint 1 (added click tool) ; git push

--- docker-sql/pipeline/ingest_data.py
+++ docker-sql/pipeline/ingest_data.py
@@ -8,52 +8,6 @@ import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
-
-# Read a sample of the data
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz')
-
-
-
-# In[18]:
-
-
-df.head()
-
-
-# In[19]:
-
-
-df.dtypes
-
-
-# In[20]:
-
-
-df.shape
-
-
-# In[21]:
-
-
-len(df)
-
-
-# In[22]:
-
-
-df
-
-
-# In[23]:
-
-
-df['VendorID']
-
-
-# In[24]:
-
-
 dtype = {
     "VendorID": "Int64",
     "passenger_count": "Int64",
@@ -77,6 +31,23 @@ parse_dates = [
     "tpep_pickup_datetime",
     "tpep_dropoff_datetime"
 ]
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+
+
+# Read a sample of the data
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz')
+
 
 df = pd.read_csv(
     prefix + 'yellow_tripdata_2021-01.csv.gz',
